17c6926477b6dfb74a6abccdaeba6b689ccc556b
aqasim23 <57294858+aqasim23@users.noreply.github.com>
2022-02-21 02:24:14 +0500

    state manegement starting

--- lib/Widgets/product_item.dart
+++ lib/Widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/Screens/product_detail_screen.dart';
 
 class productItem extends StatelessWidget {
   final String id;
@@ -16,9 +17,17 @@ class productItem extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              productDetailsScreen.routeName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'package:flutter_complete_guide/Screens/product_detail_screen.dart';
 import 'package:flutter_complete_guide/Screens/products_overview_screen.dart';
+import 'Screens/products_overview_screen.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: 'Lato',
+        ),
+        home: productsOverScreen(),
+        routes: {
+          productDetailsScreen.routeName: (ctx) => productDetailsScreen(),
+        },
       ),
-      home: productsOverScreen(),
     );
   }
 }
